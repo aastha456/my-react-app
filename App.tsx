fe414606d409e7808cb46e08dc552f907123b005
aastha456 <tamrakaraastha7@gmail.com>
2026-02-01 21:47:21 +0545

    Format the document

--- App.tsx
+++ App.tsx
@@ -2,15 +2,23 @@ import { createRoot } from "react-dom/client";
 import Book from "./Book";
 
 const App = () => {
-    return (
-        <div>
-            <h1>My Book List</h1>
-            <Book title="The Alchemist" author="Paulo Coelho" description="A book about following your dreams." />
-            <Book title="1984" author="George Orwell" description="A dystopian novel about surveillance and control." />
-            <Book title="To Kill a Mockingbird" author="Harper Lee"/>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>My Book List</h1>
+      <Book
+        title="The Alchemist"
+        author="Paulo Coelho"
+        description="A book about following your dreams."
+      />
+      <Book
+        title="1984"
+        author="George Orwell"
+        description="A dystopian novel about surveillance and control."
+      />
+      <Book title="To Kill a Mockingbird" author="Harper Lee" />
+    </div>
+  );
+};
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
